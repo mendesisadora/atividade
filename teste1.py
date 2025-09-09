7f426e68db913b65a7310b6faccd6261c7534cc6
pereiradevel <icaro.santana@aluno.ce.gov.br>
2025-09-09 11:06:05 -0300

    algo

---
+++ teste1.py
@@ -0,0 +1,37 @@
+# #Questão 1 – Saudação personalizada (Função simples)
+# def saudacao(nome):
+#     return f"Olá,{nome}! Seja bem-vindo ao curso de lógica de programação."
+# print(saudacao("ana"))
+
+#Questão 2 – Par ou Ímpar (Condicional + Função)
+# def par_ou_impar(numero):
+#     if numero % 2 == 0:
+#         return "par"
+#     else: return "impar"
+# print(f"O número 1 é {par_ou_impar(1)}")
+
+#Questão 3 – Maior de dois números (Condicional)
+# n1 = float(input("Digite meu primeiro valor:"))
+# n2 = float(input("Digite meu segundo valor:"))
+# if n1 > n2:
+#     print(f"{n1} é maior que {n2}")
+# if n2 > n1:
+#     print(f"{n2} é maior que {n1}")
+# if  n1 == n2:
+#     print(f"{n1} e {n2} são iguais")     
+
+#Questão 4 – Tabuada (Repetição)
+# numero= int(input("Digite um valor:"))
+# for i in range(1, 11):
+
+#Questão 5 – Contagem regressiva (Laço)
+# for contagem in range(1,11):
+#     print(contagem)
+# print("EXPLOSÃO!!!!!!!!!!!!!!!!!")
+
+#Questão 6 – Média de notas (Função + Repetição)
+for notas in range(1, 11):
+    notas = float(input("Digite uma nota:"))
+def medias(notas):
+    return "notas/10"   
+print(medias(notas))
