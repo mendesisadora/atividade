79ce7fd6b918218f34971ea2684ae7f283dba42b
pereiradevel <icaro.santana@aluno.ce.gov.br>
2025-09-18 08:54:28 -0300

    lista

--- teste1.py
+++ teste1.py
@@ -30,8 +30,46 @@
 # print("EXPLOSÃO!!!!!!!!!!!!!!!!!")
 
 #Questão 6 – Média de notas (Função + Repetição)
-for notas in range(1, 11):
-    notas = float(input("Digite uma nota:"))
-def medias(notas):
-    return "notas/10"   
-print(medias(notas))
+# def calcular_media():
+#     quntidade_notas = int(input("Quantas notas você quer calcular a média?: "))
+#     total = 0
+#     for i in range(quntidade_notas):
+#         nota = float(input(f"Digite a {i+1}ª nota: "))
+#         total += nota
+#     media = total / quntidade_notas
+#     return media
+# media = calcular_media()
+# print(f"A média das notas é: {media}")
+
+# #Questão 7 – Fatorial (Repetição + Função)
+# numero = int(input("Digite um número para que seja exibido seu fatorial:"))
+# fatorial = 1
+# for i in range(1, numero + 1):
+#     fatorial *= i
+# print(f"O fatorial de {numero} é {fatorial}")
+
+# #Questão 8 – Verificar vogais (Função + String)
+# ContarVogais = input("Digite uma palavra:")
+# vogais = "aeiou"
+# contador = 0
+# for letra in ContarVogais : 
+#     if letra in vogais:
+#         contador += 1
+# print(f"A palavra {ContarVogais} possui {contador} vogais.")
+
+# #Questão 9 – Jogo de adivinhação (Laço + Condicional)
+# def jogo_adivinhacao():
+#     numero_secreto = 7
+#     chute = 0
+#     tentativas = 0
+#     print("Jogo de Adivinhação")
+#     while chute != numero_secreto:
+#         chute = int(input("Digite um número entre 1 e 10: "))
+#         tentativas += 1
+#         if chute == numero_secreto:
+#             print(f"Parabéns! Você acertou em {tentativas} tentativas!")
+#         elif chute < numero_secreto:
+#             print("O número é MAIOR. Tente novamente.")
+#         else:
+#             print("O número é MENOR. Tente novamente.")
+# jogo_adivinhacao()
